0ad656c0aa1140b42b84469b4498f33a4cc5e388
s1432g <dmpshivamkumar@gmail.com>
2022-09-24 07:27:29 +0530

    added some css

--- index.js
+++ index.js
@@ -160,7 +160,7 @@ function clickHandler(e){
         else{
             var [ctr, nextDate] = getNextPalindromeDate(date);
              resultRef.innerText = `the next palindrome date is ${nextDate.day}-${nextDate.month}-${nextDate.year} ,  you missed it by ${ctr} days!`
-            //resultRef.innerText =( 'the next palindrome date is'+ nextDate.day+'-'+nextDate.month+'-'+nextDate.year + ' you missed it by' + ctr +'days!')
+            
         }
     }
 
